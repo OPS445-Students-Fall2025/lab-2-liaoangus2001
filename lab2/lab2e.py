57342aebec16249e12f9096fcfd27b992dd9b900
liaoangus2001 <lliao16@myseneca.ca>
2025-09-18 22:25:01 -0400

    Lab 2 completed - add scripts and lab2output.txt

---
+++ lab2/lab2e.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# Author: Liu-Min Liao
+# Author ID: liao16
+# Date Created: 2025/09/18
+
+timer = 10
+while timer != 0:
+    print(timer)
+    timer = timer - 1
+print('blast off!')
---
+++ lab2/lab2f.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# Author: Liu-Min Liao
+# Author ID: liao16
+# Date Created: 2025/09/18
+
+import sys
+
+timer = int(sys.argv[1])
+while timer != 0:
+    print(timer)
+    timer = timer - 1
+print('blast off!')
